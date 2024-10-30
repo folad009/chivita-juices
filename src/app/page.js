007b572db4363fa39a70256d6a2800f2f6749de6
Fola <folaremidixon@gmail.com>
2024-10-30 22:18:15 +0100

    added loding screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,26 @@
 import InstagramGallery from "@/components/common/InstagramCommunity";
 import StoreLocator from "@/components/common/StoreLocator";
 import ChivitaTrads from "@/components/home/ChivitaTrads";
-//import HomeFeature from "@/components/home/HomeFeature";
 import HomeProducts from "@/components/home/HomeProducts";
 import HomeSlider from "@/components/home/HomeSlider";
-
+import LoadingScreen from "@/components/common/LoadingScreen";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true); // Initialize loading state
+
+  useEffect(() => {
+    // Simulate a loading effect
+    const timer = setTimeout(() => setLoading(false), 1500); // Adjust timeout as needed
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
+
   return (
     <>
+      {loading && <LoadingScreen />}
       <HomeSlider />
       <HomeProducts />
-      <ChivitaTrads/>
+      <ChivitaTrads />
       <InstagramGallery />
       <StoreLocator />
     </>
