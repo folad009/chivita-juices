ebb3fe4da1bdb84a7bc6532837cfcdaa4ac7627d
Fola <folaremidixon@gmail.com>
2025-01-13 09:41:20 +0100

    update on chivita product pages

--- src/app/products/chivita-active/page.jsx
+++ src/app/products/chivita-active/page.jsx
@@ -6,15 +6,15 @@ const ChivitaActive = () => {
     <>
     <section className="relative py-10 overflow-hidden bg-black sm:py-16 lg:py-24 xl:py-32">
     <div className="absolute inset-0">
-        <img className="object-cover w-full h-full md:object-right md:scale-100 md:origin-top-right" src="/assets/images/chivita-active-bg-pg.jpg" alt="" />
+        <img className="object-cover w-full h-full lg:object-right md:scale-100 md:origin-top-right" src="/assets/images/chivita-active-background-new.jpg" alt="" />
     </div>
 
     <div className="absolute inset-0 hidden bg-gradient-to-l md:block from-black to-transparent"></div>
 
     <div className="absolute inset-0 block bg-black/70 md:hidden"></div>
 
-    <div className="relative px-2 mx-8 sm:px-4 lg:px-2 max-w-7xl">
-        <div className="text-center md:w-2/3 lg:w-1/2 xl:w-1/2 md:text-right ml-auto">
+    <div className="relative px-2 mx-8 sm:px-4 lg:px-2 max-w-7xl h-[300px]">
+        <div className="text-center md:w-2/3 lg:w-1/2 xl:w-1/2 md:text-right ml-auto mt-10">
             <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">Chivita Active</h2>
             <p className="mt-4 text-base text-gray-200">Chivita Active is our innovative masterstroke, launched in 2010 as a
             unique fruit juice blend of six citrus fruits. We extended the brand
--- src/app/products/chivita-ice-tea/page.jsx
+++ src/app/products/chivita-ice-tea/page.jsx
@@ -6,15 +6,15 @@ const ChivitaIceTea = () => {
     <>
     <section className="relative py-10 overflow-hidden bg-black sm:py-16 lg:py-24 xl:py-32">
     <div className="absolute inset-0">
-        <img className="object-cover w-full h-full md:object-right lg:scale-95 md:origin-top-right" src="/assets/images/chivita-ice-tea-bg.jpg" alt="chivita ice tea" />
+        <img className="object-cover w-full h-full md:object-right lg:scale-100 md:origin-top-right" src="/assets/images/chivita-ice-tea-bg.jpg" alt="chivita ice tea" />
     </div>
 
     <div className="absolute inset-0 hidden bg-gradient-to-l md:block from-black to-transparent"></div>
 
     <div className="absolute inset-0 block bg-black/70 md:hidden"></div>
 
-    <div className="relative px-2 mx-8 sm:px-4 lg:px-2 max-w-7xl">
-        <div className="text-center md:w-2/3 lg:w-1/2 xl:w-1/2 md:text-right ml-auto">
+    <div className="relative px-2 mx-8 sm:px-4 lg:px-2 max-w-7xl h-[300px]">
+        <div className="text-center md:w-2/3 lg:w-1/2 xl:w-1/2 md:text-right ml-auto mt-10">
             <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">Chivita Ice Tea</h2>
             <p className="mt-4 text-base text-gray-200">Chivita Ice Tea Lemon is a blend of natural tea extracts with the
               fruity lemon flavour. Launched in 2009, Chivita Ice Tea is the
