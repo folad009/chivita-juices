3dec987c7154f62fa41a1c9dcafc5263c56becfd
Fola <folaremidixon@gmail.com>
2025-02-04 10:01:54 +0100

    footer updated

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -21,38 +21,43 @@ const ContactPage = () => {
           <div className="relative z-10 flex-1 lg:flex lg:items-center lg:-mx-6">
             <div className=" lg:w-1/2 lg:mx-6 pl-10">
               <div className="mt-6 space-y-8 md:mt-8">
-                <div className="flex items-start -mx-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-9 h-9 mx-2"
-                    fill="#f9f9f9"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="1"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                  </svg>
+                <div>
+                  <h6 className="text-3xl pl-1 text-white">Address</h6>
+                  <div className="flex items-start -mx-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-9 h-9 mx-2"
+                      fill="#f9f9f9"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="1"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
 
-                  <span
-                    className="mx-2 text-[#f9f9f9] truncate w-72 text-[18px]"
-                    style={{ fontFamily: "Azo Sans, sans-serif" }}
-                  >
-                    Chivita|Hollandia
-                    <br />
-                    14, Chivita Avenue, <br /> Ajao Estate, Lagos Nigeria.
-                  </span>
+                    <span
+                      className="mx-2 text-[#f9f9f9] truncate w-72 text-[18px]"
+                      style={{ fontFamily: "Azo Sans, sans-serif" }}
+                    >
+                      Chivita|Hollandia
+                      <br />
+                      14, Chivita Avenue, <br /> Ajao Estate, Lagos Nigeria.
+                    </span>
+                  </div>
                 </div>
-
-                <div className="flex items-start -mx-2">
+              
+                <div>
+                  <h6 className="text-3xl pl-1 text-white">Phone Numbers</h6>
+                  <div className="flex items-start -mx-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="w-7 h-7 mx-2"
@@ -75,141 +80,148 @@ const ContactPage = () => {
                     <a href="tel:+234014662300">+234 01 466 2300</a>
                   </span>
                 </div>
-
-                <div className="flex items-start -mx-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-7 h-7 mx-2"
-                    fill="#f9f9f9"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                    />
-                  </svg>
-
-                  <span
-                    className="mx-2 text-[#f9f9f9] truncate  text-[18px]"
-                    style={{ fontFamily: "Azo Sans, sans-serif" }}
-                  >
-                    <a href="tel:+234017000890">+234 01 700 0890</a>
-                  </span>
-                </div>
-
-                <div className="flex items-start -mx-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 mx-2"
-                    fill="#f9f9f9"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                    />
-                  </svg>
-
-                  <span
-                    className="mx-2 text-[#f9f9f9] truncate w-72  text-[18px]"
-                    style={{ fontFamily: "Azo Sans, sans-serif" }}
-                  >
-                    <a href="tel:+2349062875224">+234 906 287 5224</a>
-                  </span>
-                </div>
-
-                <div className="flex items-start -mx-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 mx-2"
-                    fill="#f9f9f9"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                    />
-                  </svg>
-
-                  <span
-                    className="mx-2 text-[#f9f9f9] truncate w-72  text-[18px]"
-                    style={{ fontFamily: "Azo Sans, sans-serif" }}
-                  >
-                    <a href="https://wa.link/7s14am">
-                      +234 916 997 9519 (WhatsApp)
-                    </a>
-                  </span>
-                </div>
-
-                <div className="flex items-start -mx-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-7 h-7 mx-2 "
-                    fill="#f9f9f9"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                    />
-                  </svg>
-
-                  <span
-                    className="mx-2 text-[#f9f9f9] truncate w-72  text-[18px]"
-                    style={{ fontFamily: "Azo Sans, sans-serif" }}
-                  >
-                    <a
-                      href="mailto:chicares@chilimited.com"
-                      className="hover:text-white"
+                  <div className="flex items-start -mx-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-7 h-7 mx-2"
+                      fill="#f9f9f9"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
                     >
-                      chicares@chilimited.com
-                    </a>
-                  </span>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                      />
+                    </svg>
+
+                    <span
+                      className="mx-2 text-[#f9f9f9] truncate  text-[18px]"
+                      style={{ fontFamily: "Azo Sans, sans-serif" }}
+                    >
+                      <a href="tel:+234017000890">+234 01 700 0890</a>
+                    </span>
+                  </div>
+
+                  <div className="flex items-start -mx-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-2"
+                      fill="#f9f9f9"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                      />
+                    </svg>
+
+                    <span
+                      className="mx-2 text-[#f9f9f9] truncate w-72  text-[18px]"
+                      style={{ fontFamily: "Azo Sans, sans-serif" }}
+                    >
+                      <a href="tel:+2349062875224">+234 906 287 5224</a>
+                    </span>
+                  </div>
+
+                  <div className="flex items-start -mx-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-2"
+                      fill="#f9f9f9"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                      />
+                    </svg>
+
+                    <span
+                      className="mx-2 text-[#f9f9f9] truncate w-72  text-[18px]"
+                      style={{ fontFamily: "Azo Sans, sans-serif" }}
+                    >
+                      <a href="https://wa.link/7s14am">
+                        +234 916 997 9519 (WhatsApp)
+                      </a>
+                    </span>
+                  </div>
                 </div>
 
-                <div className="flex items-start -mx-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-9 h-9 mx-2"
-                    fill="#f9f9f9"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="1"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                  </svg>
-                  <span
-                    className="mx-2 text-[#f9f9f9] truncate w-72 text-[18px] pt-1"
-                    style={{ fontFamily: "Azo Sans, sans-serif" }}
-                  >
-                    <a
-                      href="/storelocator"
+                <div>
+                  <h6 className="text-3xl pl-1 text-white">Email Address</h6>
+                  <div className="flex items-start -mx-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-7 h-7 mx-2 "
+                      fill="#f9f9f9"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
                     >
-                      Find our Chivita|Hollandia Shoppe
-                    </a>
-                  </span>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                      />
+                    </svg>
+
+                    <span
+                      className="mx-2 text-[#f9f9f9] truncate w-72  text-[18px]"
+                      style={{ fontFamily: "Azo Sans, sans-serif" }}
+                    >
+                      <a
+                        href="mailto:chicares@chilimited.com"
+                        className="hover:text-white"
+                      >
+                        chicares@chilimited.com
+                      </a>
+                    </span>
+                  </div>
                 </div>
+                  
+                <div>
+                  <h6 className="text-3xl pl-1 text-white">Find our stores</h6>
+                    <div className="flex items-start -mx-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-9 h-9 mx-2"
+                        fill="#f9f9f9"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="1"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                      </svg>
+                      <span
+                        className="mx-2 text-[#f9f9f9] truncate w-72 text-[18px] pt-1"
+                        style={{ fontFamily: "Azo Sans, sans-serif" }}
+                      >
+                        <a
+                          href="/storelocator"
+                        >
+                         Chivita|Hollandia Shoppe
+                        </a>
+                      </span>
+                    </div>
+                </div>
+                
               </div>
 
               <div className="mt-6 md:mt-8">
